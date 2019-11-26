e98795c7c64cc54b769746d6921c30ac3809bf9a
ing12xunder@gmail.com <ing12xunder@gmail.com>
2019-11-26 08:43:21 -0500

    Paul Saravia
    Version 1.1, he hecho la creacion de test de diferentes modulos.

---
+++ test/controlador/Curso/MostrarCursoTest.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador.Curso;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class MostrarCursoTest {
+    
+    public MostrarCursoTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of processRequest method, of class MostrarCurso.
+     */
+    @Test
+    public void testProcessRequest() throws Exception {
+        System.out.println("processRequest");
+        HttpServletRequest request = null;
+        HttpServletResponse response = null;
+        MostrarCurso instance = new MostrarCurso();
+        instance.processRequest(request, response);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of doGet method, of class MostrarCurso.
+     */
+    @Test
+    public void testDoGet() throws Exception {
+        System.out.println("doGet");
+        HttpServletRequest request = null;
+        HttpServletResponse response = null;
+        MostrarCurso instance = new MostrarCurso();
+        instance.doGet(request, response);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of doPost method, of class MostrarCurso.
+     */
+    @Test
+    public void testDoPost() throws Exception {
+        System.out.println("doPost");
+        HttpServletRequest request = null;
+        HttpServletResponse response = null;
+        MostrarCurso instance = new MostrarCurso();
+        instance.doPost(request, response);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getServletInfo method, of class MostrarCurso.
+     */
+    @Test
+    public void testGetServletInfo() {
+        System.out.println("getServletInfo");
+        MostrarCurso instance = new MostrarCurso();
+        String expResult = "";
+        String result = instance.getServletInfo();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
