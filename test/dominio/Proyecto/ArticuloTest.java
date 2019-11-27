1537a951da4b0fca0643af5ed36eda557bd1fd38
JesusTacca <job1and2ramza@gmail.com>
2019-11-27 14:04:08 -0500

    Test Dominio Proyecto

---
+++ test/dominio/Proyecto/ArticuloTest.java
@@ -0,0 +1,36 @@
+
+package dominio.Proyecto;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ArticuloTest {
+    
+    public ArticuloTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+    
+}
